f113d0b8a5e02d72994afa1fec203300c9bc574a
김동진 <dongjin403@naver.com>
2025-01-22 20:30:42 +0900

    feat : view, exception 클래스 생성

---
+++ src/main/java/custom/CustomException.java
@@ -0,0 +1,9 @@
+package custom;
+
+public class CustomException extends RuntimeException {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    public CustomException(String message) {
+        super(ERROR_PREFIX + message);
+    }
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,15 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static final String LOTTO_PRICE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String input(String msg) {
+        System.out.println(msg);
+        return Console.readLine();
+    }
+}
