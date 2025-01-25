b309511501f8686a0b902bb5adba39f4e66818c2
김동진 <dongjin403@naver.com>
2025-01-25 16:03:49 +0900

    feat : OutputView 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static final String LOTTO_PRICE = "구입금액을 입력해 주세요.";
-    public static final String LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    public static final String LOTTO_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
     public String input(String msg) {
         System.out.println(msg);
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,16 @@
 package view;
 
 public class OutputView {
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public void printf(String message) {
+        System.out.printf(message);
+    }
+
+    public void printLottoCount(int count) {
+        println("\n" + count + "개를 구매했습니다.");
+    }
 }
