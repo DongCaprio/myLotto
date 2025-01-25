d1b017f0dd213bc6e64644121415ba189f805398
김동진 <dongjin403@naver.com>
2025-01-25 23:55:15 +0900

    feat : 당첨번호를 통해 로또의 숫자 몇개가 당첨됐는지(몇등인지) 파악하는 기능 추가

--- src/main/java/lotto/WinningInfo.java
+++ src/main/java/lotto/WinningInfo.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import custom.CustomException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WinningInfo {
 
     private final WinningNumber winningNumber;
     private final BonusNumber bonusNumber;
 
-    public WinningInfo(WinningNumber winningNumber, BonusNumber bonusNumber) {
+    private WinningInfo(WinningNumber winningNumber, BonusNumber bonusNumber) {
         this.winningNumber = winningNumber;
         this.bonusNumber = bonusNumber;
     }
@@ -21,5 +23,37 @@ public class WinningInfo {
         if (winningNumber.getWinningNumber().contains(bonusNumber.getBonusNumber())) {
             throw new CustomException("로또 당첨 번호와 보너스 번호는 중복될 수 없습니다");
         }
+    }
+
+    public LottoRank specifyLottoRank(Lotto lotto) {
+        int winningCount = checkWinningCount(lotto);
+        if (winningCount == 6) {
+            return LottoRank.FIRST;
+        }
+        if (winningCount == 5) {
+            return checkBonusNumber(lotto);
+        }
+        if (winningCount == 4) {
+            return LottoRank.FOURTH;
+        }
+        if (winningCount == 3) {
+            return LottoRank.FIFTH;
+        }
+        return null;
+    }
+
+    private int checkWinningCount(Lotto lotto) {
+        Set<Integer> winningSet = new HashSet<>(winningNumber.getWinningNumber());
+        Set<Integer> lottoSet = new HashSet<>(lotto.getNumbers());
+        winningSet.retainAll(lottoSet);
+        return winningSet.size();
+    }
+
+    private LottoRank checkBonusNumber(Lotto lotto) {
+        boolean matchBonus = lotto.getNumbers().contains(bonusNumber.getBonusNumber());
+        if (matchBonus) {
+            return LottoRank.SECOND;
+        }
+        return LottoRank.THIRD;
     }
 }
