59500186bede319e69d645dfd5134e7cca03e556
김동진 <dongjin403@naver.com>
2025-01-25 16:08:12 +0900

    feat : 로또 생성 후 당첨번호, 보너스 번호 입력받는 로직 추가

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
+import lotto.BonusNumber;
+import lotto.Lotto;
+import lotto.WinningInfo;
+import lotto.WinningNumber;
 import verify.LottoCount;
 import view.InputView;
+import view.OutputView;
 
 public class LottoController {
 
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     private <T> T handleRetryOnError(Supplier<T> method) {
         try {
@@ -19,6 +27,12 @@ public class LottoController {
 
     public void run() {
         LottoCount lottoCount = inputPrice();
+        outputView.printLottoCount(lottoCount.getLottoCount());
+        List<Lotto> lottos = makeLottos(lottoCount.getLottoCount());
+        for (Lotto lotto : lottos) {
+            outputView.println(lotto.toString());
+        }
+        WinningInfo winningInfo = WinningInfo.from(inputWinningNumber(), inputBonusNumber());
     }
 
     private LottoCount inputPrice() {
@@ -28,4 +42,25 @@ public class LottoController {
         });
     }
 
+    private WinningNumber inputWinningNumber() {
+        return handleRetryOnError(() -> {
+            String inputWinningNumbers = inputView.input(InputView.LOTTO_NUMBER);
+            return WinningNumber.from(inputWinningNumbers);
+        });
+    }
+
+    private BonusNumber inputBonusNumber() {
+        return handleRetryOnError(() -> {
+            String inputBonusNumbers = inputView.input(InputView.BONUS_NUMBER);
+            return BonusNumber.from(inputBonusNumbers);
+        });
+    }
+
+    private List<Lotto> makeLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(Lotto.makeRandomLotto());
+        }
+        return lottos;
+    }
 }
