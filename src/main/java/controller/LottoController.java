8ff3754618e6571558edde3417b5e2c5497682eb
김동진 <dongjin403@naver.com>
2025-02-03 16:09:01 +0900

    refactor : InputView 의존성 제거

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -52,21 +52,21 @@ public class LottoController {
 
     private LottoCount inputPrice() {
         return handleRetryOnError(() -> {
-            String input = inputView.inputMessage(InputView.LOTTO_PRICE);
+            String input = inputView.inputLottoPrice();
             return LottoCount.from(input);
         });
     }
 
     private WinningNumber inputWinningNumber() {
         return handleRetryOnError(() -> {
-            String inputWinningNumbers = inputView.inputMessage(InputView.LOTTO_NUMBER);
+            String inputWinningNumbers = inputView.inputLottoNumber();
             return WinningNumber.from(inputWinningNumbers);
         });
     }
 
     private LottoNumber inputBonusNumber() {
         return handleRetryOnError(() -> {
-            String inputBonusNumbers = inputView.inputMessage(InputView.BONUS_NUMBER);
+            String inputBonusNumbers = inputView.inputBonusNumber();
             return LottoNumber.from(inputBonusNumbers);
         });
     }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,12 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static final String LOTTO_PRICE = "구입금액을 입력해 주세요.";
-    public static final String LOTTO_NUMBER = "\n당첨 번호를 입력해 주세요.";
-    public static final String BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    private static final String LOTTO_PRICE = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
 
-    public String inputMessage(String message) {
-        System.out.println(message);
+    public String inputLottoPrice() {
+        System.out.println(LOTTO_PRICE);
+        return Console.readLine();
+    }
+
+    public String inputLottoNumber() {
+        System.out.println(LOTTO_NUMBER);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        System.out.println(BONUS_NUMBER);
         return Console.readLine();
     }
 }
